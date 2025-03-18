382c317987d3e84092af06a9850b069e45f8a394
4irdy <fpopal@holyfamily.edu>
2025-03-17 22:56:39 -0400

    Update urls.py

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include  # Add 'include' to import app URLs
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('portfolio.urls')),  # Include the URLs for the portfolio app
 ]
