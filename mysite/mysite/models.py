f9f943cdb876e6259a48bc0e588656db62beb9c8
4irdy <fpopal@holyfamily.edu>
2025-04-24 05:13:57 -0400

    Update models.py

--- mysite/mysite/models.py
+++ mysite/mysite/models.py
@@ -2,9 +2,8 @@ from django.db import models
 
 class Photo(models.Model):
     title = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='photos/')  # This will store the image in a folder called 'photos/'
-    description = models.TextField()
-    category = models.CharField(max_length=50)
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to='photos/')
 
     def __str__(self):
         return self.title
