f82172276d62b1fccf663ca572525d73b48632e3
4irdy <fpopal@holyfamily.edu>
2025-04-24 05:26:27 -0400

    Update models.py

--- mysite/mysite/models.py
+++ mysite/mysite/models.py
@@ -1,8 +1,16 @@
 from django.db import models
 
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
 class Photo(models.Model):
     title = models.CharField(max_length=100)
-    description = models.TextField(blank=True)
+    date = models.DateField()
+    location = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     image = models.ImageField(upload_to='photos/')
 
     def __str__(self):
